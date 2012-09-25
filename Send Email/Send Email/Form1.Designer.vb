00475b6eb55b9e4f63e487a4af2af7f43c58bb0a
Enes Genç <enesgenc@gmail.com>
2012-09-25 22:37:50 +0300

    First Release

---
+++ Send Email/Send Email/Form1.Designer.vb
@@ -0,0 +1,175 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSend = New System.Windows.Forms.Button
+        Me.txtFrom = New System.Windows.Forms.TextBox
+        Me.txtTo = New System.Windows.Forms.TextBox
+        Me.txtSubject = New System.Windows.Forms.TextBox
+        Me.txtMessage = New System.Windows.Forms.TextBox
+        Me.lblSender = New System.Windows.Forms.Label
+        Me.lblTo = New System.Windows.Forms.Label
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.txtPass = New System.Windows.Forms.TextBox
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip
+        Me.lblStatus = New System.Windows.Forms.ToolStripStatusLabel
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnSend
+        '
+        Me.btnSend.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnSend.Location = New System.Drawing.Point(422, 243)
+        Me.btnSend.Name = "btnSend"
+        Me.btnSend.Size = New System.Drawing.Size(69, 28)
+        Me.btnSend.TabIndex = 5
+        Me.btnSend.Text = "Send"
+        Me.btnSend.UseVisualStyleBackColor = True
+        '
+        'txtFrom
+        '
+        Me.txtFrom.Location = New System.Drawing.Point(74, 12)
+        Me.txtFrom.Name = "txtFrom"
+        Me.txtFrom.Size = New System.Drawing.Size(200, 20)
+        Me.txtFrom.TabIndex = 0
+        '
+        'txtTo
+        '
+        Me.txtTo.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtTo.Location = New System.Drawing.Point(74, 38)
+        Me.txtTo.Name = "txtTo"
+        Me.txtTo.Size = New System.Drawing.Size(417, 20)
+        Me.txtTo.TabIndex = 2
+        '
+        'txtSubject
+        '
+        Me.txtSubject.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtSubject.Location = New System.Drawing.Point(74, 64)
+        Me.txtSubject.Name = "txtSubject"
+        Me.txtSubject.Size = New System.Drawing.Size(417, 20)
+        Me.txtSubject.TabIndex = 3
+        '
+        'txtMessage
+        '
+        Me.txtMessage.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtMessage.Location = New System.Drawing.Point(12, 90)
+        Me.txtMessage.Multiline = True
+        Me.txtMessage.Name = "txtMessage"
+        Me.txtMessage.Size = New System.Drawing.Size(479, 147)
+        Me.txtMessage.TabIndex = 4
+        '
+        'lblSender
+        '
+        Me.lblSender.AutoSize = True
+        Me.lblSender.Font = New System.Drawing.Font("00249", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.lblSender.Location = New System.Drawing.Point(11, 11)
+        Me.lblSender.Name = "lblSender"
+        Me.lblSender.Size = New System.Drawing.Size(57, 33)
+        Me.lblSender.TabIndex = 2
+        Me.lblSender.Text = "Sender:"
+        '
+        'lblTo
+        '
+        Me.lblTo.AutoSize = True
+        Me.lblTo.Font = New System.Drawing.Font("00249", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.lblTo.Location = New System.Drawing.Point(12, 37)
+        Me.lblTo.Name = "lblTo"
+        Me.lblTo.Size = New System.Drawing.Size(31, 33)
+        Me.lblTo.TabIndex = 2
+        Me.lblTo.Text = "To:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("00249", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 63)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(61, 33)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Subject:"
+        '
+        'txtPass
+        '
+        Me.txtPass.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtPass.Location = New System.Drawing.Point(280, 12)
+        Me.txtPass.Name = "txtPass"
+        Me.txtPass.Size = New System.Drawing.Size(211, 20)
+        Me.txtPass.TabIndex = 1
+        Me.txtPass.UseSystemPasswordChar = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.lblStatus})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 274)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(503, 22)
+        Me.StatusStrip1.TabIndex = 6
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'lblStatus
+        '
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(39, 17)
+        Me.lblStatus.Text = "Status"
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(503, 296)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.txtPass)
+        Me.Controls.Add(Me.txtMessage)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblTo)
+        Me.Controls.Add(Me.lblSender)
+        Me.Controls.Add(Me.txtSubject)
+        Me.Controls.Add(Me.txtTo)
+        Me.Controls.Add(Me.txtFrom)
+        Me.Controls.Add(Me.btnSend)
+        Me.Name = "MainForm"
+        Me.Text = "Send Mail"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnSend As System.Windows.Forms.Button
+    Friend WithEvents txtFrom As System.Windows.Forms.TextBox
+    Friend WithEvents txtTo As System.Windows.Forms.TextBox
+    Friend WithEvents txtSubject As System.Windows.Forms.TextBox
+    Friend WithEvents txtMessage As System.Windows.Forms.TextBox
+    Friend WithEvents lblSender As System.Windows.Forms.Label
+    Friend WithEvents lblTo As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtPass As System.Windows.Forms.TextBox
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents lblStatus As System.Windows.Forms.ToolStripStatusLabel
+
+End Class
---
+++ Send Email/Send Email/MainForm.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Net.Mail
+Public Class MainForm
+    Private Sub btnSend_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSend.Click
+        Try
+            Dim SmtpServer As New SmtpClient()
+            Dim mail As New MailMessage()
+            SmtpServer.EnableSsl = True
+            SmtpServer.Credentials = New Net.NetworkCredential(txtFrom.Text, txtPass.Text)
+            SmtpServer.Port = 587
+            SmtpServer.Host = "smtp.gmail.com"
+            mail = New MailMessage()
+            mail.From = New MailAddress(txtFrom.Text)
+            mail.To.Add(txtTo.Text)
+            mail.Subject = txtSubject.Text
+            mail.Body = txtMessage.Text
+            SmtpServer.Send(mail)
+            lblStatus.Text = "Mail SENT"
+            lblStatus.ForeColor = Color.Green
+        Catch ex As Exception
+            lblStatus.Text = "Error, Details also in clipboard"
+            lblStatus.ForeColor = Color.Red
+            MsgBox(ex.ToString)
+            Clipboard.SetText(ex.ToString)
+        End Try
+    End Sub
+
+   
+    Private Sub MainForm_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        lblStatus.Text = "Ready"
+    End Sub
+End Class
